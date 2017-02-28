c30c3edd4e9719806336295e4f1d5f2679bd8c5f
eric.pennequin <eric.pennequin@cefimmac17.home>
2017-02-28 11:24:07 +0100

    https://github.com/eric140/WP-Canvas.git

---
+++ wp-Canvas.php
@@ -0,0 +1,24 @@
+/*Description
+*@link
+*@since         1.0.0
+*@package         wp_Canvas
+*
+*@worpress plugin
+*Plugin Name: WP Canvas and color
+*Plugin URI : https://github.com/eric140/WP-Canvas.git
+*Description: Integrate Canvas with many functions into you Wordpress
+*Version: 1.0.0
+*Author: Eric Pennequin
+*Author URI:
+*License: GPL-2+
+*License URI:
+*Text Domain: wp-Canvas
+*Domain Path: Languages
+*
+*
+*
+*
+*
+*
+*
+*/
